76ea1a3dc93d37bb8d438ef6364e173ec5760c3c
Muckilein <JuliaWessolleck@t-online.de>
2024-02-24 13:08:20 +0100

    fixes

--- src/app/all-todos/all-todos.component.ts
+++ src/app/all-todos/all-todos.component.ts
@@ -12,13 +12,15 @@ import { Router } from '@angular/router';
 })
 export class AllTodosComponent implements OnInit {
   todos: any = [];
-  contacts:any=[];
-  detailedTask :any ={};
-  taskIndex =0;
+  contacts: any = [];
+  detailedTask: any = {};
+  taskIndex = 0;
   showDetail = false;
+  newTodo=false;
   subtask = "";
   error = '';
-  openContactList:boolean = false;
+  colors=['#d8ebf8','#9deb58','#cf3886','#b131e1','#7537d1','#3e76dd','#38dbd5']
+  openContactList: boolean = false;
   constructor(private http: HttpClient, private router: Router) { }
 
 
@@ -31,7 +33,7 @@ export class AllTodosComponent implements OnInit {
     } catch (e) {
       this.error = 'Fehler beim Laden!';
     }
-  } 
+  }
 
 
   loadTodos() {
@@ -69,23 +71,19 @@ export class AllTodosComponent implements OnInit {
     }
   }
 
-  openDetails(task:any, index :number){
-    this.detailedTask =task ;
+  openDetails(task: any, index: number) {
+    this.detailedTask = task;
     this.taskIndex = index;
     this.showDetail = true;
   }
-  back(){
+  back() {
     this.showDetail = false;
+    this.newTodo = false;
   }
 
-  async save(){
-    this.todos[this.taskIndex] = this.detailedTask;
-    console.log(this.todos);
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
+  setTask() {
     const raw = JSON.stringify({
-      "id":this.detailedTask.id,
+      "id": this.detailedTask.id,
       "title": this.detailedTask.title,
       "description": this.detailedTask.description,
       "date": this.detailedTask.date,
@@ -94,10 +92,34 @@ export class AllTodosComponent implements OnInit {
       "checked": this.detailedTask.checked,
       "prio": this.detailedTask.prio,
       "state": this.detailedTask.state,
-      "subtask":this.detailedTask.subtask,
-      "assignments" : this.detailedTask.assignments
+      "subtask": this.detailedTask.subtask,
+      "assignments": this.detailedTask.assignments
     });
+    return raw;
+  }
 
+  makeEmptyTodo() {
+    this.newTodo = true;  
+    this.showDetail=true; 
+    this.detailedTask.id = 0;
+    this.detailedTask.title = "";
+    this.detailedTask.description = "";
+    this.detailedTask.date = "";
+    this.detailedTask.category = "";
+    this.detailedTask.color = this.colors[Math.floor(Math.random()*7)];
+    this.detailedTask.checked = "";
+    this.detailedTask.prio = "";
+    this.detailedTask.state = "";
+    this.detailedTask.subtask = [];
+    this.detailedTask.assignments = [];
+  }
+
+  async makeTodo() {
+   
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization",'Token ' + localStorage.getItem('token'))
+    const raw = this.setTask();
     const requestOptions: RequestInit = {
       method: 'POST',
       headers: myHeaders,
@@ -105,41 +127,80 @@ export class AllTodosComponent implements OnInit {
       redirect: 'follow'
     };
     try {
-      let resp = await fetch(environment.baseUrl+"/editTodoAPI/", requestOptions);
+      let resp = await fetch(environment.baseUrl + "/createTodoAPI/", requestOptions);
       let json = await resp.json();
-      
+      this.todos.push(json);
       // TODO: Redirect
-      this.router.navigateByUrl('/todos');
-    } catch(e){
+     // this.router.navigateByUrl('/todos');
+    } catch (e) {
       // Show error message
       console.error(e);
 
     }
   }
 
-  addSubtask(){ 
-  console.log(this.subtask);
-  let sub = {'id': 'null', 'title':this.subtask ,'checked':false};
-  this.detailedTask.subtask.push(sub);  
-  this.subtask = "";
-  console.log(this.detailedTask);
+  refreshStateSubtasks(){
+  this.detailedTask
+
   }
-  addAssigments(){
-    this.openContactList = true;    
+
+  register(){
+    this.router.navigateByUrl('/register');
   }
-  addC(c:any){
+
+  async save() {
+    this.todos[this.taskIndex] = this.detailedTask;
+    console.log(this.detailedTask);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization",'Token ' + localStorage.getItem('token'))
+    const raw = this.setTask();
+    const requestOptions: RequestInit = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    try {
+      let resp = await fetch(environment.baseUrl + "/createTodoAPI/" + this.detailedTask.id + "/", requestOptions);
+      let json = await resp.json();
+      this.todos[this.taskIndex] = json;
+      this.detailedTask = json;
+      console.log(this.detailedTask);
+
+      // TODO: Redirect
+      //this.router.navigateByUrl('/todos');
+    } catch (e) {
+      // Show error message
+      console.error(e);
+
+    }
+  }
+
+  addSubtask() {
+    console.log(this.subtask);
+    if (this.subtask != "") {
+      console.log(this.subtask)
+      let sub = { 'id': 'null', 'title': this.subtask, 'checked': false };
+      this.detailedTask.subtask.push(sub);
+      this.subtask = "";
+      console.log(this.detailedTask);
+    }
+  }
+  addAssigments() {
+    this.openContactList = true;
+  }
+  addC(c: any) {
     this.openContactList = false;
-    let ass = {'id': c['id'], 'name':c['name']};
+    let ass = { 'id': c['id'], 'name': c['name'] };
     let exist = false;
     let actualAssigments = this.detailedTask.assignments;
-    actualAssigments.forEach((a:any)=>{
-     if(a['id'] == c['id'])
-     { 
-      exist = true;
-     }
+    actualAssigments.forEach((a: any) => {
+      if (a['id'] == c['id']) {
+        exist = true;
+      }
     })
-    if(!exist)
-    {this.detailedTask.assignments.push(ass);}
+    if (!exist) { this.detailedTask.assignments.push(ass); }
   }
 
 }
